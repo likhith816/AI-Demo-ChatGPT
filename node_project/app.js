c2326b1e72b87bdcce6abfb4bdb18e5f36fafc6c
Parepalli Likhith Sai <162948828+likhith816@users.noreply.github.com>
2025-05-10 10:27:29 +0530

    Delete node_project directory

--- node_project/app.js
+++
@@ -1,4 +0,0 @@
-const math = require('./math');
-
-console.log("Addition: ", math.add(5, 3));
-console.log("Subtraction: ", math.subtract(5, 3));
--- node_project/events.js
+++
@@ -1,8 +0,0 @@
-const EventEmitter = require('events');
-const eventEmitter = new EventEmitter();
-
-eventEmitter.on('greet', (name) => {
-    console.log(`Hello, ${name}! Welcome to Node.js.`);
-});
-
-eventEmitter.emit('greet', 'Likhith');
